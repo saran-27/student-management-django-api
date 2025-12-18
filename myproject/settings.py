6530fa09b3b4c6bb0b13f89efdd2d37ef13f0165
Saravanan_27 <jacksaran243@gmail.com>
2025-12-19 00:38:42 +0530

    switch database to sqlite for render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'student_performance',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
