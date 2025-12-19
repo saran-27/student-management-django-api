2943f7fc6e60fe9c33ab7ea9bf11211bdc1e375b
Saravanan_27 <jacksaran243@gmail.com>
2025-12-19 11:32:05 +0530

    Fixed cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ORIGINS =  ["http://localhost:3000",  # for dev
+    "https://https://student-management-rouge-ten.vercel.app/",]
 
 # Application definition
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+
 ]
 
 ROOT_URLCONF = 'myproject.urls'
