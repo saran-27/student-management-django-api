a2697a19422362a16f9832c24c05d04c4d5cb043
Saravanan_27 <jacksaran243@gmail.com>
2025-12-19 11:08:01 +0530

    Fixed migrations and database setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student_management',
+    'student_management.apps.StudentManagementConfig',
     'corsheaders',
 
 ]
