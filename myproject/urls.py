22521f7d950afb60372ee8d74988b778c75cc825
Saravanan_27 <jacksaran243@gmail.com>
2025-12-19 11:42:06 +0530

    Changes in urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('studentrank/',include("student_management.urls"))
+path('student/',include("student_management.urls"))
 ]
